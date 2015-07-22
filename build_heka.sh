95a2f63a50121e50720f119e64f7643f5810b8bd
Paul Bonser <misterpib@gmail.com>
2015-07-22 13:58:39 -0500

    Try doing .deb build on CircleCI

--- build_heka.sh
+++ build_heka.sh
@@ -2,6 +2,7 @@
 
 : ${CIRCLE_BUILD_NUM:=`git describe --always`}
 : ${CIRCLE_ARTIFACTS:=`pwd`}
+: ${BUILD_TYPE:=TGZ}
 
 goat deps
 
@@ -13,7 +14,6 @@ rsync -a --exclude-from .gitignore `pwd`/ .goat/deps/src/github.com/mozilla-serv
 pushd .goat/deps/src/github.com/mozilla-services/heka
 echo 'add_external_plugin(git https://github.com/OwnLocal/heka-plugins :local)' > cmake/plugin_loader.cmake
 source build.sh
-cpack -G TGZ -D CPACK_PACKAGE_FILE_NAME=heka-${CIRCLE_BUILD_NUM}
+cpack -G ${BUILD_TYPE} -D CPACK_PACKAGE_FILE_NAME=heka-${CIRCLE_BUILD_NUM}
 cp heka-${CIRCLE_BUILD_NUM}.tar.gz ${CIRCLE_ARTIFACTS}/
 popd
-
