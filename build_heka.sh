003e42d6813867a050b8049eec91d17320e061e8
Paul Bonser <misterpib@gmail.com>
2015-07-22 14:24:59 -0500

    Use proper package naming and add build number as debian revision

--- build_heka.sh
+++ build_heka.sh
@@ -14,6 +14,6 @@ rsync -a --exclude-from .gitignore `pwd`/ .goat/deps/src/github.com/mozilla-serv
 pushd .goat/deps/src/github.com/mozilla-services/heka
 echo 'add_external_plugin(git https://github.com/OwnLocal/heka-plugins :local)' > cmake/plugin_loader.cmake
 source build.sh
-cpack -G ${BUILD_TYPE} -D CPACK_PACKAGE_FILE_NAME=heka-${CIRCLE_BUILD_NUM}
+cpack -G ${BUILD_TYPE} -D CPACK_PACKAGE_FILE_NAME=heka_0.9.2-${CIRCLE_BUILD_NUM}-amd64 -D CPACK_DEBIAN_PACKAGE_VERSION=0.9.2-${CIRCLE_BUILD_NUM}
 cp heka-${CIRCLE_BUILD_NUM}.* ${CIRCLE_ARTIFACTS}/
 popd
