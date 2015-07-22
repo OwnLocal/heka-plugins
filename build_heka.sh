3d7c0b44739b5b0f67a7e772287358ffedea794b
Paul Bonser <misterpib@gmail.com>
2015-07-22 14:28:10 -0500

    use new build name when moving to circle artifacts dir

--- build_heka.sh
+++ build_heka.sh
@@ -14,6 +14,7 @@ rsync -a --exclude-from .gitignore `pwd`/ .goat/deps/src/github.com/mozilla-serv
 pushd .goat/deps/src/github.com/mozilla-services/heka
 echo 'add_external_plugin(git https://github.com/OwnLocal/heka-plugins :local)' > cmake/plugin_loader.cmake
 source build.sh
-cpack -G ${BUILD_TYPE} -D CPACK_PACKAGE_FILE_NAME=heka_0.9.2-${CIRCLE_BUILD_NUM}-amd64 -D CPACK_DEBIAN_PACKAGE_VERSION=0.9.2-${CIRCLE_BUILD_NUM}
-cp heka-${CIRCLE_BUILD_NUM}.* ${CIRCLE_ARTIFACTS}/
+BUILD_NAME=heka_0.9.2-${CIRCLE_BUILD_NUM}-amd64
+cpack -G ${BUILD_TYPE} -D CPACK_PACKAGE_FILE_NAME=${BUILD_NAME} -D CPACK_DEBIAN_PACKAGE_VERSION=0.9.2-${CIRCLE_BUILD_NUM}
+cp ${BUILD_NAME}.* ${CIRCLE_ARTIFACTS}/
 popd
